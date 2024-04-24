33509900f635f182c46d8a340b2e8ea3c7632dd9
CadeWebb <cadewebb16@gmail.com>
2024-04-23 20:41:12 -0700

    frontend-react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,59 @@
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import About from './pages/About';
+import Support from './pages/Support';
+import NoPage from './pages/NoPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Barrydorr from './pages/Barrydorr';
+import Ugurdogan from './pages/Ugurdogan';
+import Parisakaveh from './pages/Parisakaveh';
+import Yingkhaiteh from './pages/Yingkhaiteh';
+import Professor from './components/Professor';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 0,
+        },
+    },
+});
+
+const professors = [
+    {
+        fullName: "Ugur Dogan", description: "Professor", path:"/ugurdogan"
+    },
+    {
+        fullName: "Parisa Kaveh", description: "PhD", path: "/parisakaveh"
+    }
+]
+
+function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="about" element={<About />} />
+                        <Route path="support" element={<Support />} />
+                        {professors.map(professor => (
+                            <Route key={professor.path} path={professor.path} element={<Professor fullName={professor.fullName} description={professor.description} />} />
+                        ))
+                        
+                        }
+                        {/* <Route path="Barrydorr" element={<Barrydorr />} /> */}
+                        {/* <Route path="Ugurdogan" element={<Professor fullName="Ugur Dogan" description={"PRofessor"} />} /> */}
+                        {/* <Route path="Parisakaveh" element={<Parisakaveh />} /> */}
+                        {/* <Route path="Yingkhaiteh" element={<Yingkhaiteh />} /> */}
+                        <Route path="*" element={<NoPage />} />
+                    </Route>
+                </Routes>
+            </Router>
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,44 @@
+const About = () => {
+    return (
+        <>
+            <main>
+                <section className="container">
+                    <h2> About Us</h2>
+                    <p>
+                        Welcome to RateMySDSU Professor, a website for rating your experiences with professors.
+                    </p>
+                </section>
+                <section className="container">
+                    <h3> Website Overview</h3>
+                    <p>
+                    We are SDSU students making a ratemyprof website for SDSU professors. 
+                    This was started by Patricia Munn, Cade Webb, Brenden Ludkevicz, and 
+                    Anabel Sanchez for our COMPE 561 Project.
+                    </p>
+
+                    
+                </section>
+                <section className="container">
+                    
+
+                    <h3>Technologies Used</h3>
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                        <li>Material UI</li>
+                        
+                        <li>React</li>
+                        <li>Python</li>
+                        <li>Django</li>
+                        <li>MySQL</li>
+                        
+                    </ul>
+                </section>
+                
+            </main>
+        </>
+    );
+};
+export default About;
