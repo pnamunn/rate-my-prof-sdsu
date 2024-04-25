32497e4c0fc65e2a12cfda3b6bf24d3422984599
Brenden Ludkevicz <hawks0919@gmail.com>
2024-04-24 17:19:11 -0700

    Review Posts

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class ReviewPost(models.Model):
+    class Rating(models.IntegerChoices):
+        one = 1
+        two = 2
+        three = 3
+        four = 4
+        five = 5 
+    
+    professorName = models.CharField(max_length=100)
+    rating = models.IntegerField(choices=Rating)
+    review = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.professorName
