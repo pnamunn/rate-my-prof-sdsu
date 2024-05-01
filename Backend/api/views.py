30f480c514b895c313abbd7aa6b46ee30f59451c
Brenden Ludkevicz <hawks0919@gmail.com>
2024-04-30 21:31:32 -0700

    Added delete post model

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -7,3 +7,8 @@ from .serializers import ReviewPostSerializer
 class ReviewPostListCreate(generics.ListCreateAPIView):
     queryset = ReviewPost.objects.all()
     serializer_class = ReviewPostSerializer
+
+class ReviewPostListRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = ReviewPost.objects.all()
+    serializer_class = ReviewPostSerializer
+    lookup_field = 'pk'
