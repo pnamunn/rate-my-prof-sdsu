dd8e5d8203e8993cce6591cc430de7aba527d310
pnamunn <pnamunn@gmail.com>
2024-05-01 21:06:45 -0700

    added featured profs on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-
-
+import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Grid';
 import Search from '../components/Search'
 
 
@@ -12,6 +12,34 @@ const Home = () => {
                     <h1>Welcome to Rate My SDSU Professor</h1>
                     <h3>Search for a professor</h3>
                     <Search/>
+
+                    <h3>Featured professors:</h3>
+
+                    <Grid
+                    container
+                    spacing={1}
+                    >
+                    <Grid item xs={6}>
+                        <Avatar src='/jediteh.png' sx={{ width: 150, height: 150 }}/>
+                        <h4 style={{"text-align":"center"}}>Prof. Teh</h4>
+                    </Grid> 
+                    <Grid item xs={6}>
+                        <Avatar src='/ugurdogan.jpeg' sx={{ width: 150, height: 150 }}/>
+                        <h4 style={{"text-align":"center"}}>Prof. Dogan</h4>
+                        
+                    </Grid> 
+                    <Grid item xs={6}>
+                        <Avatar src='/laser_eyes_dorr.jpeg' sx={{ width: 150, height: 150 }}/>
+                        <h4 style={{"text-align":"center"}}>Prof. Dorr</h4>
+                        
+                    </Grid> 
+                    <Grid item xs={6}>
+                        <Avatar src='/arnold.jpg' sx={{ width: 150, height: 150 }}/>
+                        <h4 style={{"text-align":"center"}}>Prof. Arnold</h4>
+                        
+                    </Grid> 
+
+                    </Grid>
                     
                 </div>
             
