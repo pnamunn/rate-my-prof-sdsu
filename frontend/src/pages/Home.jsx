ca889777d3cb0e8a86a6fe5bc6bc09ee333f8e30
pnamunn <pnamunn@gmail.com>
2024-05-01 21:18:36 -0700

    Prof links

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
 import Search from '../components/Search'
 
 
@@ -20,22 +21,34 @@ const Home = () => {
                     spacing={1}
                     >
                     <Grid item xs={6}>
+                        <Link href="/yingkhaiteh">
                         <Avatar src='/jediteh.png' sx={{ width: 150, height: 150 }}/>
+                        </Link>
                         <h4 style={{"text-align":"center"}}>Prof. Teh</h4>
                     </Grid> 
                     <Grid item xs={6}>
+                        <Link href="/ugurdogan">
                         <Avatar src='/ugurdogan.jpeg' sx={{ width: 150, height: 150 }}/>
+                        </Link>
                         <h4 style={{"text-align":"center"}}>Prof. Dogan</h4>
                         
                     </Grid> 
                     <Grid item xs={6}>
+                        <Link href="/barrydorr">
+
                         <Avatar src='/laser_eyes_dorr.jpeg' sx={{ width: 150, height: 150 }}/>
                         <h4 style={{"text-align":"center"}}>Prof. Dorr</h4>
+                        </Link>
+
                         
                     </Grid> 
                     <Grid item xs={6}>
+                        <Link href="/kennetharnold">
+
                         <Avatar src='/arnold.jpg' sx={{ width: 150, height: 150 }}/>
                         <h4 style={{"text-align":"center"}}>Prof. Arnold</h4>
+                        </Link>
+
                         
                     </Grid> 
 
