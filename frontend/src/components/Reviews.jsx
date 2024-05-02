a8b88dae6d1b51b0586c259131a5b43dc27c17f9
CadeWebb <cadewebb16@gmail.com>
2024-05-01 20:55:42 -0700

    added props to reviews component

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -1,9 +1,10 @@
 /* This will be the component that renders the existing reviews for a professor */
-
+import {useState} from 'react'
 import { useGetReviews } from "../hooks/useGetReview";
 
-const Reviews = () => {
+const Reviews = (props) => {
     const { data: reviews, error, isLoading } = useGetReviews();
+    
 
     
 
@@ -17,8 +18,9 @@ const Reviews = () => {
         <>
             <h3>Existing Reviews</h3>
             
+            
             <ul>{reviews.map((review) => 
-                <li key={review.id}>{review.professorName}. Rating = {review.rating}. {review.review}</li>
+                <li key={review.id}>Rating = {review.rating}. {review.review}</li>
                 
                 )}
             </ul>
